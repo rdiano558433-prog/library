68511451ce0e1bd1ddfc26373cfd45e3a3fc7c51
Rumar Diano <r.diano.558433@umindanao.edu.ph>
2026-05-04 15:36:02 -0700

    Fix: Rename Books to books directory and add index view

--- resources/views/Books/index.blade.php
+++ resources/views/Books/index.blade.php
@@ -1,3 +1,6 @@
+<div>
+    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+</div>
 @extends('layouts.app')
 @section('title', 'Books')
 @section('page-title', 'Book Catalog')
